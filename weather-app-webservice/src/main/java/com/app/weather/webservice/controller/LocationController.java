651db6e9d2361bda431a7855c088ce1abe773235
Bhaskar Gogoi <bhaskargogoi24@gmail.com>
2020-01-25 16:09:30 -0500

    added endpoint to get multiple data based on what users want

--- weather-app-webservice/src/main/java/com/app/weather/webservice/controller/LocationController.java
+++ weather-app-webservice/src/main/java/com/app/weather/webservice/controller/LocationController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.websocket.server.PathParam;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/weather")
@@ -30,4 +31,12 @@ public class LocationController {
         return locationService.getWeather(lat, lon);
     }
 
+    @GetMapping("/getWeatherData/{num}")
+    public List<WeatherResponse> getWeatherData(@PathVariable("num") int num) throws IOException {
+        List<Location> locations = getLocations(num);
+        List<WeatherResponse> weatherReport = locations.stream()
+                .map(location -> getWeather(location.getLat(), location.getLon()))
+                .collect(Collectors.toList());
+        return weatherReport;
+    }
 }
